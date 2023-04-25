60f5b8b68d3e7404c008715a88655e9a31dd65cf
orneryhippo <jesse.cassill@gmail.com>
2023-04-25 13:12:02 -0400

    annoying

--- chunker.js
+++ chunker.js
@@ -39,8 +39,9 @@ function extractValuesFromJsonArray(jsonTextArray, jsonKey) {
 function prettyPrintJson(jsonInput) {
     try {
         // Parse the input JSON string
-        let jsonString = jsonInput[0];
-        const jsonObj = JSON.parse(jsonString);
+        const jsonArray = JSON.parse(jsonInput);
+        const firstElement = jsonArray[0];
+        const jsonObj = JSON.parse(firstElement);
         console.log(jsonObj);
         // Pretty-print the JSON object with a 2-space indentation
         const prettyJson = JSON.stringify(jsonObj, null, 2);
