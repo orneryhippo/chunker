a2c36cad011d01e08b2654fdbe4540f777484508
orneryhippo <jesse.cassill@gmail.com>
2023-04-28 10:35:31 -0400

    Added tuple count

--- wordfreq.js
+++ wordfreq.js
@@ -39,8 +39,24 @@ function displayWordFreqs() {
         wordFreqs = countWordTuples(words, wordsPerTuple);
     }
     const outputDiv = document.getElementById("output");
-    const prettyPrintedWordCounts = JSON.stringify(wordFreqs, null, 2);
-    outputDiv.innerHTML = `<pre>${prettyPrintedWordCounts}</pre>`;
+    const sortedWordFrequencies = sortByFrequency(wordFreqs);
+    // const prettyPrintedWordCounts = JSON.stringify(wf, null, 2);
+    // outputDiv.innerHTML = `<pre>${prettyPrintedWordCounts}</pre>`;
+    
+    const tableHTML = createTable(sortedWordFrequencies);
+    outputDiv.innerHTML = tableHTML;
 }
-
+function sortByFrequency(wordTuples) {
+    const sortedTuples = Object.entries(wordTuples).sort((a, b) => b[1] - a[1]);
+    return sortedTuples;
+  }
+  function createTable(sortedWordFrequencies) {
+    let table = '<table border="0" cellspacing="0" cellpadding="5"><tr><th>Word Tuple</th><th>Frequency</th></tr>';
+    sortedWordFrequencies.forEach(([wordTuple, frequency]) => {
+      table += `<tr><td>${wordTuple}</td><td>${frequency}</td></tr>`;
+    });
+    table += '</table>';
+    return table;
+  }
+  
 
