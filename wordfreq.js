6b6af786d73dd49785d0fb8c85a998bcbde6c9db
orneryhippo <jesse.cassill@gmail.com>
2023-04-28 10:16:17 -0400

    Added tuple count

--- wordfreq.js
+++ wordfreq.js
@@ -1,8 +1,8 @@
 "use strict";
 // wordfreq.js
-function wordFreq(text) {
+function wordFreq(words) {
     const wordFreqs = {};
-    const words = text.toLowerCase().match(/\b\w+\b/g) || [];
+    
     for (const word of words) {
         if (wordFreqs[word]) {
             wordFreqs[word]++;
@@ -13,10 +13,34 @@ function wordFreq(text) {
     }
     return wordFreqs;
 }
+// wordfreq.js
+function countWordTuples(words, n) {
+    const wordTuples = {};
+  
+    for (let i = 0; i <= words.length - n; i++) {
+      const tuple = words.slice(i, i + n).join(' ');
+      if (wordTuples[tuple]) {
+        wordTuples[tuple]++;
+      } else {
+        wordTuples[tuple] = 1;
+      }
+    }
+  
+    return wordTuples;
+}
 function displayWordFreqs() {
     const inputText = document.getElementById("inputText").value;
-    const wordFreqs = wordFreq(inputText);
+    const wordsPerTuple = Number(document.getElementById("wordsPerTuple").value);
+    const words = inputText.toLowerCase().match(/\b\w+\b/g) || [];
+    let wordFreqs = {};
+    if (wordsPerTuple == 1) {
+        wordFreqs = wordFreq(words);
+    } else {
+        wordFreqs = countWordTuples(words, wordsPerTuple);
+    }
     const outputDiv = document.getElementById("output");
     const prettyPrintedWordCounts = JSON.stringify(wordFreqs, null, 2);
     outputDiv.innerHTML = `<pre>${prettyPrintedWordCounts}</pre>`;
 }
+
+
